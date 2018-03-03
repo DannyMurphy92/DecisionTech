7751cb2214f66ef2aa9fa6198c649f897f724c99
Danny Murphy <danny.murphy@gmail.com>
2018-03-03 10:16:35 +0000

    Added basket singleton

---
+++ Shop/src/Shop.Core/Models/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Models
+{
+    public class Basket
+    {
+        private static Basket instance;
+
+        private Basket()
+        {}
+
+        public static Basket Instance()
+        {
+            return instance ?? (instance = new Basket());
+        }
+    }
+}
---
+++ Shop/test/Shop.Core.UnitTests/Models/BasketTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Core.Models;
+
+namespace Shop.Core.UnitTests.Models
+{
+    [TestFixture]
+    public class BasketTestFixture
+    {
+
+        [Test]
+        public void Instance_WhenCalledMultipleTimes_ReturnsTheSameInstanceOfBasket()
+        {
+            // Arrange
+
+            // Act
+            var ins1 = Basket.Instance();
+            var ins2 = Basket.Instance();
+
+            // Assert
+            Assert.AreEqual(ins1, ins2);
+        }
+    }
+}
